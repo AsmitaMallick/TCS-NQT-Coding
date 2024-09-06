8b794cfb78981685777304acfc073607327c6d64
Asmita Mallick <114489333+AsmitaMallick@users.noreply.github.com>
2024-09-06 10:16:51 +0530

    Update code.cpp

--- Accenture/2. Find Mid Element/code.cpp
+++ Accenture/2. Find Mid Element/code.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
-void find_mid_element(std::vector<int> arr) {
-    std::vector<int> filtered_arr;
+using namespace std;
+void find_mid_element(vector<int> arr) {
+    vector<int> filtered_arr;
     for (int num : arr) {
         if (num >= 0) {
             filtered_arr.push_back(num);
         }
     }
-    int mid_index = (filtered_arr.size() - 1) / 2;
-    std::cout << filtered_arr[mid_index] << std::endl;
+   sort(filtered_arr.begin, filtered_arr.end);
+int n = filtered_arr.size;
+if (n % 2 != 0){
+        return arr[n / 2];}
+    else{
+return arr[(n - 1) / 2];
 }
-
+}
 int main() {
     std::vector<int> arr = {7, -3, 8, 2, -5, 10, 12};
     find_mid_element(arr);
